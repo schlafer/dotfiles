d4eb7dddba6ca4b6d12f1ced79f79d81a1241a3f
Sazid Mahmud <sazidm@proton.me>
2025-09-26 09:51:59 +0600

    add: fish

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# Initialize zoxide
+zoxide init fish | source
+
+# Aliases (same as in bash/zsh)
+alias z="__zoxide_z"
+alias zi="__zoxide_zi"
+
+# eza
+alias ls="eza --icons --group-directories-first -lh"
+alias lla="eza -lha --icons"
+alias lt="eza --tree --icons"
+alias llt="eza -lh --tree" # long view tree
+alias lS="eza -lh --sort=size" # sort by size
+alias lD="eza -lh --sort=date" # sort by date
+alias lg="eza -lh --git" # show git status
---
+++ dot_config/fish/functions/y.fish
@@ -0,0 +1,12 @@
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+
+    if test -f "$tmp"
+        set cwd (cat "$tmp")
+        if test -n "$cwd" -a "$cwd" != "$PWD"
+            cd "$cwd"
+        end
+        rm -f "$tmp"
+    end
+end
